94359a7dff5b41b3c940976f378247dfef90a49f
RancidMilkGames <rancidmilkgames@gmail.com>
2023-01-26 15:13:49 -0700

    Goes with last commit

---
+++ addons/PaletteTools/scripts/custom_picker.gd
@@ -0,0 +1,5 @@
+@tool
+extends PopupPanel
+
+@onready var color_picker: ColorPicker = $ScrollContainer/VBoxContainer/ColorPicker
+@onready var saved_palettes: ItemList = $ScrollContainer/VBoxContainer/SavedPalettes
